aaaa55d1c3c900a14f586159f52bf54a78dec963
deymon28 <dimag2811@gmail.com>
2023-10-15 21:03:55 +0300

    не працює 2

--- ranobes_parser.py
+++ ranobes_parser.py
@@ -5,7 +5,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import pickle
-
+from selenium.webdriver.common.by import By
 
 def selenium_firs_page(src):
     user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36"
@@ -20,69 +20,81 @@ def selenium_firs_page(src):
     cookies = pickle.load(open("cookies.pkl", "rb"))
     for cookie in cookies:
         driver.add_cookie(cookie)
-    time.sleep(1000)
-    pickle.dump(driver.get_cookies(), open("cookies.pkl", "wb"))
+    time.sleep(1)
+    driver.find_element(By.ID, "content").click()
+    time.sleep(1)
+    # pickle.dump(driver.get_cookies(), open("cookies.pkl", "wb"))
     content = driver.page_source.encode('utf-8').strip()
-    soup = BeautifulSoup(content, 'lxml')
-    return print(soup, "\n\n\n\n\n\n\################################\\\n\n\n\n\n\n\n\n\n\n\n")
+    # soup = BeautifulSoup(content, 'lxml')
+    # return print(soup)
 
-    # return html_content
+    return content
 
-def load_cookies():
-    with open('cookies.pkl', 'rb') as f:
-        pickle.load(f)
+
 
 
 def request_page(url):
-
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36'}
-    response = requests.get(url, headers=headers, cookies=load_cookies())
-
-    if response.status_code == 200:
-        html_content = response.content
-        return html_content
-    else:
-        print(f"Failed to retrieve HTML content for page {url}. Status code: {response.status_code}")
-        return None
+    return selenium_firs_page(url)
+    #
+    # headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36'}
+    # response = requests.get(url, headers=headers, cookies=load_cookies())
+    #
+    # if response.status_code == 200:
+    #     html_content = response.content
+    #     return html_content
+    # else:
+    #     print(f"Failed to retrieve HTML content for page {url}. Status code: {response.status_code}")
+    #     return None
 
 
 def chapter_pages(book):
     html_content = request_page(book)
+    # print(html_content)
 
     soup = BeautifulSoup(html_content, 'lxml')
-    print(soup)
+    # print(soup)
 
-    number_of_pages = soup.find("<div>", class_="pages").find_all("a")
+    number_of_pages = soup.find("div", class_="pages").find_all("a")
 
     if number_of_pages:
         last_number_of_pages = number_of_pages[-1]
+        print("\n#\n#\n#\n#\n#\n" + last_number_of_pages.text + "\n#\n#\n#\n#\n#\n")
         return int(last_number_of_pages.text)
     else:
         return print("Error: Чомусь не можливо отримати кількість сторінок(chapter_pages)")
 
 
-def parse_chapters_hrefs(number_of_pages):
+def parse_chapters_hrefs(book_url, number_of_pages):
     names_hrefs = {}
 
-    for i in range(1, number_of_pages + 1):
-        url = f"https://ranobes.com/chapters/second-life-ranker/page/{i}"
+    for i in range(2, number_of_pages + 1):
+        url = book_url + f"page/{i}"
+
 
         html_content = request_page(url)
         soup = BeautifulSoup(html_content, 'lxml')
 
-        y = 1
-        while y <= 30:
-            divs = soup.find_next("div", _class="cat_block cat_line").find("a")
-            temporarily = {divs.get('title'): divs.get('href')}
-            names_hrefs.update(temporarily)
-            y += 1
+        # y = 1
+        # while y <= 30:
+        divs = soup.find_all("div", class_="cat_block cat_line")
+        for div in divs:
+            a_tag = div.find("a")
+            if a_tag:
+                temporarily = {a_tag.get('title'): a_tag.get('href')}
+                names_hrefs.update(temporarily)
 
+            # print(divs)
+            # temporarily = {divs.get('title'): divs.get('href')}
+            # names_hrefs.update(temporarily)
+            # y += 1
+
+    print(names_hrefs)
     return names_hrefs
 
 
 def extrakt_chapter(names_hrefs):
 
-    for name, href in names_hrefs:
+    for name, href in names_hrefs.items():
         html_content = request_page(href)
         soup = BeautifulSoup(html_content, 'lxml')
 
@@ -99,7 +111,7 @@ def extrakt_chapter(names_hrefs):
         result = '\n'.join(formatted_paragraphs)
         result = name + "\n\n" + result
 
-        return name, result
+        return name, result #завершує цикл після 1 операції, потрібно виправити
 
 
 
@@ -137,10 +149,10 @@ if __name__ == '__main__':
 
     number_of_pages = chapter_pages(book)
 
-    names_hrefs = parse_chapters_hrefs(number_of_pages)
+    names_hrefs = parse_chapters_hrefs(book, number_of_pages)
 
     name, text = extrakt_chapter(names_hrefs)
 
     clean_chapter = clean_text(text)
 
-    save_chapter = save_chapter(clean_chapter, name)
+    save_chapter(clean_chapter, name)
--- tests/test_selenium.py
+++ tests/test_selenium.py
@@ -1,29 +1,146 @@
-from selenium import webdriver
-from bs4 import BeautifulSoup
+'''The updated code you provided has fixed several issues, and it looks better than the previous version. However, there are still a few improvements that can be made for better functionality and readability:
 
-def selenium_first_page(src):
-    options = webdriver.ChromeOptions()
-    options._ignore_local_proxy = True
+Error Handling: Add proper error handling to your code. Handle exceptions that might occur during web scraping, file operations, and other critical parts of your script.
 
-    driver = webdriver.Chrome(options=options)
-    driver.get(src)
+Function Naming: It's a good practice to use descriptive function names. For example, selenium_firs_page can be renamed to something like get_page_source_with_selenium to make it clear what the function does.
 
-    content = driver.page_source.encode('utf-8').strip()
-    soup = BeautifulSoup(content, 'lxml')
+Variable Naming: Avoid using variable names like y. Use descriptive names that convey the purpose of the variable.
 
-    return soup
+Loop Logic: In the parse_chapters_hrefs function, the while loop has a hardcoded limit of 30 iterations. Consider using a for loop with a fixed range instead of a while loop.
 
-if __name__ == "__main__":
-    book = "https://www.example.com/book"
+Return Values: Ensure that your functions return the appropriate values. For example, extrakt_chapter currently returns after the first iteration. If you want to collect all chapters, consider using a list to store them and return the list after the loop.
 
-    soup = selenium_first_page(book)
+Code Comments: Add comments to explain complex logic or non-trivial operations in your code. This will make it easier for others (and your future self) to understand the code.
 
-    # Extract the title of the book
-    title = soup.find('h1', class_='book-title').text
+Code Organization: Consider breaking down your code into smaller functions with specific responsibilities. This will make your code modular and easier to maintain.
 
-    # Extract the author of the book
-    author = soup.find('span', class_='book-author').text
+Here's an example of how you could structure your code with some of these improvements:
 
-    # Print the title and author of the book
-    print(f"Title: {title}")
-    print(f"Author: {author}")
+python
+Copy code
+'''
+# Import statements
+
+def get_page_source_with_selenium(url):
+    # Your Selenium logic here
+    pass
+
+def get_page_content_with_requests(url):
+    # Your requests logic here
+    pass
+
+def extract_chapters_info(html_content):
+    # Extract chapter names and URLs from the HTML content
+    pass
+
+def extract_chapter_text(html_content):
+    # Extract and format chapter text from the HTML content
+    pass
+
+def clean_text(input_text):
+    # Your text cleaning logic here
+    pass
+
+def save_chapter_to_file(text, name):
+    # Save chapter text to a file
+    pass
+
+def main():
+    book_url = "https://ranobes.com/chapters/second-life-ranker/"
+    html_content = get_page_content_with_requests(book_url)
+    chapters_info = extract_chapters_info(html_content)
+
+    for chapter_name, chapter_url in chapters_info.items():
+        chapter_html = get_page_content_with_requests(chapter_url)
+        chapter_text = extract_chapter_text(chapter_html)
+        cleaned_text = clean_text(chapter_text)
+        save_chapter_to_file(cleaned_text, chapter_name)
+
+if __name__ == '__main__':
+    main()
+'''Please note that the functions extract_chapters_info, extract_chapter_text, and others are placeholders. You should implement the logic inside these functions based on your specific requirements.
+
+
+how to implement the 4th and 5th points you suggested
+ChatGPT
+
+2 / 2
+
+Certainly! Let me guide you through implementing the 4th and 5th points, which involve improving the loop logic and ensuring proper return values in your code.
+
+4. Loop Logic:
+In the parse_chapters_hrefs function, you had a while loop with a hardcoded limit of 30 iterations. It's better to use a for loop with a fixed range for more control and readability. Here's how you can do it:
+
+python
+Copy code
+'''
+def parse_chapters_hrefs(book_url, number_of_pages):
+    names_hrefs = {}
+
+    for i in range(1, number_of_pages + 1):
+        url = f"{book_url}page/{i}"
+        html_content = request_page(url)
+        soup = BeautifulSoup(html_content, 'lxml')
+
+        chapter_divs = soup.find_all("div", class_="cat_block cat_line")
+        for div in chapter_divs:
+            a_tag = div.find("a")
+            chapter_title = a_tag.get('title')
+            chapter_link = a_tag.get('href')
+            names_hrefs[chapter_title] = chapter_link
+
+    return names_hrefs
+'''
+In this code, the loop iterates through each page and finds all the chapter links on that page. It adds each chapter's title and link to the names_hrefs dictionary.
+
+5. Return Values:
+In the extrakt_chapter function, you were returning after processing the first chapter. To collect all chapters, you can use a list to store the chapter data and return that list after the loop. Here's how you can modify the function:
+
+python
+Copy code
+'''
+def extract_chapters(chapters_data):
+    extracted_chapters = []
+
+    for name, href in chapters_data.items():
+        html_content = request_page(href)
+        soup = BeautifulSoup(html_content, 'lxml')
+
+        paragraphs = soup.select('.text p')
+        formatted_paragraphs = []
+
+        for paragraph in paragraphs:
+            cleaned_paragraph = paragraph.get_text().replace('\n', ' ')
+            formatted_paragraphs.append(cleaned_paragraph)
+
+        chapter_text = '\n'.join(formatted_paragraphs)
+        cleaned_text = clean_text(chapter_text)
+
+        extracted_chapters.append((name, cleaned_text))
+
+    return extracted_chapters
+'''
+In this modified function, extracted_chapters is a list of tuples, where each tuple contains the chapter title and its cleaned text. The function loops through all chapters, processes them, and adds them to the list. Finally, the list of extracted chapters is returned.
+
+These modifications ensure that your loops are efficient and that you collect and return all the necessary data from your functions. Remember to handle errors and edge cases appropriately for a robust implementation.
+'''
+
+def parse_chapters_hrefs(book_url, number_of_pages):
+    names_hrefs = {}
+
+    for i in range(1, number_of_pages + 1):
+        url = f"{book_url}page/{i}"
+        html_content = request_page(url)
+        soup = BeautifulSoup(html_content, 'lxml')
+
+        chapter_divs = soup.find_all("div", class_="cat_block cat_line")
+        for div in chapter_divs:
+            a_tag = div.find("a")
+            if a_tag:
+                chapter_title = a_tag.get('title')
+                chapter_link = a_tag.get('href')
+                names_hrefs[chapter_title] = chapter_link
+            else:
+                print("Warning: No chapter link found in div.")
+
+    return names_hrefs
